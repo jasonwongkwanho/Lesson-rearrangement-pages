cb2c2ea564df68e5760587cd26fc3d81734cb31f
jasonwongkwanho <214792573+jasonwongkwanho@users.noreply.github.com>
2026-06-13 17:07:33 +0800

    Use public Apps Script API URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/a/macros/pokwong.edu.hk/s/AKfycbxr1NMJjIUhtEK2jOLh3c7M4SBfhUkSPIix-ZfxomfQTcWfUbtpoacsWqBlBQI46nC_Aw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxr1NMJjIUhtEK2jOLh3c7M4SBfhUkSPIix-ZfxomfQTcWfUbtpoacsWqBlBQI46nC_Aw/exec",
   LEGACY_WEBAPP_URL: "https://script.google.com/a/macros/pokwong.edu.hk/s/AKfycbxr1NMJjIUhtEK2jOLh3c7M4SBfhUkSPIix-ZfxomfQTcWfUbtpoacsWqBlBQI46nC_Aw/exec",
   AUTO_REFRESH_MS: 15000,
   APP_NAME: "編代課系統",
