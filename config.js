c2f6226e23b1e12893846b07a0f72e51db534b2e
jasonwongkwanho <214792573+jasonwongkwanho@users.noreply.github.com>
2026-06-13 16:55:26 +0800

    Configure Apps Script API URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  API_URL: "",
-  LEGACY_WEBAPP_URL: "",
+  API_URL: "https://script.google.com/a/macros/pokwong.edu.hk/s/AKfycbxr1NMJjIUhtEK2jOLh3c7M4SBfhUkSPIix-ZfxomfQTcWfUbtpoacsWqBlBQI46nC_Aw/exec",
+  LEGACY_WEBAPP_URL: "https://script.google.com/a/macros/pokwong.edu.hk/s/AKfycbxr1NMJjIUhtEK2jOLh3c7M4SBfhUkSPIix-ZfxomfQTcWfUbtpoacsWqBlBQI46nC_Aw/exec",
   AUTO_REFRESH_MS: 15000,
   APP_NAME: "編代課系統",
   APP_SUBTITLE: "網頁同步版",
